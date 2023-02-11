c6ef58e2874f5d0dbe887d084af52cc3ea27e849
linnchoeuh <lennintendo@gmail.com>
2023-02-11 18:39:43 +0100

    [EDIT] Fix up a memory crash caused by mouse_activity

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,25 @@ int main(void)
     sf::Event event;
     std::vector<sf::Sprite> spriteDraw;
     std::vector<sf::Text> textDraw;
-    MainMenu menu(window);
+    MainMenu menu(window, event);
     // menuConstructor(spriteDraw, textDraw);
      //Music
     sf::Music music;
     set_music(music, "sfx/menu/menu_drill.ogg", 10);
     music.play();
 
-    //easter_egg
-    Player ben;
     
     menu.loadScene(NULL, NULL);
-    int i = 0;
     while (window.isOpen()) {
         window.pollEvent(event);
         if (event.type == sf::Event::Closed)
             window.close();
-        if (i == 2 || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
             window.close();
         }
-        mouse_activity(i, event, spriteDraw[1], spriteDraw[2], menu._easter_egg);
-
+        
         window.clear();
-        if (i != 0)
-            music.stop();
-        if (i == 0)
-            menu.run(NULL, NULL);
-        if (i == 1) {
-            if (ben.getSound()->getStatus() == sf::SoundSource::Stopped && sf::Keyboard::isKeyPressed(sf::Keyboard::L))
-                ben.getSound()->play();
-        }
+        menu.run(NULL, NULL);
         window.display();
 
         // fps.capFramerate();
